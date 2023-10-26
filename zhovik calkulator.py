c6bf0226c2875d0b10c5ba5b3322f5ecf8f3b1dc
undeadkage <148542044+undeadkage@users.noreply.github.com>
2023-10-26 12:35:16 +0300

    Add files via upload

---
+++ zhovik calkulator.py
@@ -0,0 +1,8 @@
+a = int(input('первое число'))
+b = int(input('второе число'))
+operation = input('operation')
+if operation == '+':
+    result = (a + b)
+elif operation == '-':
+    result = (a - b)
+print(result)
