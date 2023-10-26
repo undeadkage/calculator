24f7da6e3b02e63e06547693847b838154ff6179
undeadkage <148542044+undeadkage@users.noreply.github.com>
2023-10-26 14:27:24 +0300

    Update zhovik calkulator.py

--- zhovik calkulator.py
+++ zhovik calkulator.py
@@ -1,12 +1,22 @@
+operation= input('выберите тип операции')
 a = int(input('первое число'))
 b = int(input('второе число'))
-operation = input('operation')
+#сложение
 if operation == '+':
     result = (a + b)
+#вычитание    
 elif operation == '-':
     result = (a - b)
+#умножение
 elif operation == '*':
     result = (a * b)
+#деление
 elif operation == '/':
     result = (a / b)
+#возведение в квадрат
+elif operation == '**':
+    result = (a**b)
+    
+#вывод результата 
+print('Ответ:')
 print(result)
